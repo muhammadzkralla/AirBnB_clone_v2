cda9ea0c5ff697d566fc058adb1c7be9da5096ae
muhammadzkralla <muhammad.heshamyt@gmail.com>
2023-12-06 22:13:07 +0200

    test

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Creates a webpage at /hbnb_static/index.html.
+# Sets up a web server for deployment of web_static.
 
 apt-get update
 apt-get install -y nginx
@@ -7,33 +7,32 @@ apt-get install -y nginx
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
 echo "Holberton School" > /data/web_static/releases/test/index.html
-
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 chown -R ubuntu /data/
 chgrp -R ubuntu /data/
 
-echo 'server {
->     listen 80 default_server;
->     listen [::]:80 default_server;
->     add_header X-Served-By $HOSTNAME;
->     root   /var/www/html;
->     index  index.html index.htm;
->
->     location /hbnb_static {
->         alias /data/web_static/current;
->         index index.html index.htm;
->     }
->
->     location /redirect_me {
->         return 301 http://cuberule.com/;
->     }
->
->     error_page 404 /404.html;
->     location /404 {
->       root /var/www/html;
->       internal;
->     }
-> }' | sudo tee /etc/nginx/sites-available/default > /dev/null
+printf %s "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By $HOSTNAME;
+    root   /var/www/html;
+    index  index.html index.htm;
+
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
+
+    location /redirect_me {
+        return 301 http://cuberule.com/;
+    }
+
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}" > /etc/nginx/sites-available/default
 
 service nginx restart
