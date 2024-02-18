2e0d0e9c60089e736ac1f2feb255ab8f4e303203
muhammadzkralla <muhammad.heshamyt@gmail.com>
2024-02-18 21:07:53 +0200

    States List

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,27 +1,24 @@
 #!/usr/bin/python3
-"""Starts listening to
-0.0.0.0:5000
+"""Starts
+listening to 0.0.0.0:5000
 """
-
-from flask import Flask, render_template
 from models import storage
-
+from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    states = storage.all("State")
+    return render_template("7-states_list.html", states=states)
+
+
 @app.teardown_appcontext
-def teardown_db(exception):
-    """Method to remove the current SQLAlchemy Session"""
+def teardown(exc):
     storage.close()
 
 
-@app.route('/states_list', strict_slashes=False)
-def states_list():
-    """Display a HTML page with a list of all State objects"""
-    states = storage.all("State").values()
-    return render_template('7-states_list.html', states=states)
-
-
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host="0.0.0.0")
